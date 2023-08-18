b8f9e1f6d6f564f96f98b3c1a774f8eb6bb584cb
Dev-ArthurFernandes <arthurdesousafernandes@gmail.com>
2023-08-18 13:06:38 -0300

    feat: Change main color

--- src/styles/Global.tsx
+++ src/styles/Global.tsx
@@ -84,8 +84,8 @@ export const GlobalStyle = ({ children }: GlobalStyleProps): JSX.Element => {
 
   const theme = createTheme({
     colors: {
-      brand1: "#623CEA",
-      brand2: "#311E75",
+      brand1: "#01FDF6",
+      brand2: "#00D0D1",
 
       grey0: "#0A0A0B",
       grey1: "#121214",
